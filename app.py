4e4f36005f8f6ce7ce7fa8f7b8e5e97bb8ad4d93
TELAGAMSETTI SAI TEJA <48620462+Sai999teja@users.noreply.github.com>
2021-05-04 19:18:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, url_for, redirect, render_template
+import pickle
+import numpy as np
+import joblib
+import sklearn
+
+app = Flask(__name__)
+
+from sklearn.ensemble import GradientBoostingClassifier
+
+model = pickle.load(open('fly_model.pkl', 'rb'))
+model2 = pickle.load(open('fly_model2.pkl', 'rb'))
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final = [np.array(int_features)]
+    print(int_features)
+    print(final)
+    prediction1 = model.predict(final)
+    prediction2 = model2.predict(final)
+    prediction1 = str(prediction1)
+    prediction2 = str(prediction2)
+
+    return render_template('index.html', pred='Fly Ash Generation and Utilization are as follows', result1=prediction1,
+                           result2=prediction2)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
